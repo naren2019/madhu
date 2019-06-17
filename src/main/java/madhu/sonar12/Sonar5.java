0953d8132790b4204ac03cd7eedea2badcfaafea
naren2019 <naren.mstr@yahoo.in>
2019-06-17 12:15:16 +0530

    commited

---
+++ src/main/java/madhu/sonar12/Sonar5.java
@@ -0,0 +1,16 @@
+package madhu.sonar12;
+
+public class Sonar5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int n=10;
+	for(int i=0;i<n;i++)
+	{
+		System.out.println(i);
+	}
+		
+
+	}
+
+}
